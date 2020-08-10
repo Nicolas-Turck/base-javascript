bedd741d5dbb3acdfa87cdcefaaef8715da5c46e
eder <nicolas.turck@gmail.com>
2020-08-10 19:48:23 +0200

    first commit

---
+++ test-js.js
@@ -0,0 +1,54 @@
+/* test code javascript cours openclassroom */
+
+
+//declare and assign a variablejavascript
+let numbersOfCats = 2;
+let numbersOfDogs = 4;
+
+// modify variable 
+
+numbersOfCats = 3;
+
+// Addition and subtraction
+
+let numbersOfPets = numbersOfCats + numbersOfDogs;
+
+let numbersOfPets2 = numbersOfDogs - numbersOfCats;
+
+numbersOfCats += 2; //add 2 to variable 
+numbersOfDogs -= 2 // substract to variable 
+
+numbersOfDogs ++; // to add  1
+numbersOfDogs --; // subtract 1
+
+
+// Multiplication and division
+
+let costPerProduct = 20;
+let numberOfProducts = 5;
+ 
+let totalCost = costPerProduct * numberOfProducts; // multiply variable 
+ 
+let averageCostPerProduct = totalCost / numberOfProducts; // divose variable 
+
+let numberOfCats = 2;
+
+numberOfCats *= 6;  // numberOfCats =  2*6 = 12; 
+
+numberOfCats /= 3;  // numberOfCats = 12/3 = 4;
+
+
+//exersises 1
+// Calculate totalShowTime here
+// =====================================
+let episodeTime = 45;
+let commercialTime = 5;
+
+let totalsEpisode = numberOfSeasons * numberOfEpisodes;
+
+let totalEpisodeAndCommercialTime = episodeTime + commercialTime;
+
+let totalShowTime = totalsEpisode * totalEpisodeAndCommercialTime;
+// =====================or ================
+
+let totalShowTime = (episodeTime + commercialTime) * numberOfSeasons * numberOfEpisodes;
