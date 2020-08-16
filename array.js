03cd0603cc959ddc68d3bb95155f63973bb50b3c
eder <nicolas.turck@gmail.com>
2020-08-16 09:42:40 +0200

    add array exemple screenshot

--- array.js
+++ array.js
@@ -1,6 +1,9 @@
 // array
 let array = [] // create array
 let client = ['eder', 'gwen', 'thiago']; // create array with items
+client.push('innaya'); // add items in end of array
+client.unshift('toto'); // add items in first posotion of array
+client.pop() // delete end element of array
 
 // create many items
 let client1 = "paul";
@@ -15,3 +18,6 @@ console.log(client[1]);// display itels in array with index
 
 console.log(listeClients);//display array
 console.log(listeClients[2]);// display itels in array with index
+
+console.log(client.length); // display number of elemnts in array
+
